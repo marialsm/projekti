ed5ccfcb2b532d6bfc5cc3a45da0abb8c7ae28f9
Maria <sm.marria@gmail.com>
2021-11-23 20:34:21 +0200

    Importoidaan Forms, ReactiveForms ja HttpClient

--- ang-pro/src/app/app.component.ts
+++ ang-pro/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/* import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang-pro';
-}
+} */
+
+//importoidaan tähän moduulit
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+
+export class AppModule { }
